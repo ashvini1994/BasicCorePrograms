ad8064d79848cddeb753b39691d1084ebdfcc5d7
Ashu <ashusarnaik11@gmail.com>
2021-11-29 09:59:32 +0530

    FlipCoin

--- BasicCoreProgram1/BasicCoreProgram1/Program.cs
+++ BasicCoreProgram1/BasicCoreProgram1/Program.cs
@@ -6,11 +6,22 @@ using System.Threading.Tasks;
 
 namespace BasicCoreProgram1
 {
-    class Program
+    class FlipCoin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome");
+            Console.WriteLine("1.FlipCoin \n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.Flip(Convert.ToInt32(Console.ReadLine()));
+                    break;
+                    //case 2:
+                    //   FlipCoin flipCoin1 = new FlipCoin();
+                    //  fl
+            }
         }
     }
 }
